a145f75ed2fcf07a9e9b57ddc4f9bc843d048542
98WuG <gerald@geraldwu.com>
2022-01-26 23:59:55 -0800

    Don't deploy linkerd

--- terraform/modules/kube-proxmox/main.tf
+++ terraform/modules/kube-proxmox/main.tf
@@ -112,61 +112,6 @@ resource "null_resource" "provisioner" {
   ]
 }
 
-resource "tls_private_key" "trustanchor_key" {
-  algorithm   = "ECDSA"
-  ecdsa_curve = "P256"
-}
-
-resource "tls_self_signed_cert" "trustanchor_cert" {
-  key_algorithm         = tls_private_key.trustanchor_key.algorithm
-  private_key_pem       = tls_private_key.trustanchor_key.private_key_pem
-  validity_period_hours = 87600
-  is_ca_certificate     = true
-
-  subject {
-    common_name    = "identity.linkerd.cluster.local"
-    street_address = []
-  }
-
-  allowed_uses = [
-    "crl_signing",
-    "cert_signing",
-    "server_auth",
-    "client_auth"
-  ]
-}
-
-resource "tls_private_key" "issuer_key" {
-  algorithm   = "ECDSA"
-  ecdsa_curve = "P256"
-}
-
-resource "tls_cert_request" "issuer_req" {
-  key_algorithm   = tls_private_key.issuer_key.algorithm
-  private_key_pem = tls_private_key.issuer_key.private_key_pem
-
-  subject {
-    common_name    = "identity.linkerd.cluster.local"
-    street_address = []
-  }
-}
-
-resource "tls_locally_signed_cert" "issuer_cert" {
-  cert_request_pem      = tls_cert_request.issuer_req.cert_request_pem
-  ca_key_algorithm      = tls_private_key.trustanchor_key.algorithm
-  ca_private_key_pem    = tls_private_key.trustanchor_key.private_key_pem
-  ca_cert_pem           = tls_self_signed_cert.trustanchor_cert.cert_pem
-  validity_period_hours = 8760
-  is_ca_certificate     = true
-
-  allowed_uses = [
-    "crl_signing",
-    "cert_signing",
-    "server_auth",
-    "client_auth"
-  ]
-}
-
 resource "helm_release" "cilium" {
   name       = "cilium"
   repository = "https://helm.cilium.io"
@@ -182,50 +127,105 @@ resource "helm_release" "cilium" {
   ]
 }
 
-resource "helm_release" "linkerd" {
-  name       = "linkerd"
-  repository = "https://helm.linkerd.io/stable"
-  chart      = "linkerd2"
+# resource "tls_private_key" "trustanchor_key" {
+#   algorithm   = "ECDSA"
+#   ecdsa_curve = "P256"
+# }
 
-  depends_on = [
-    helm_release.cilium
-  ]
+# resource "tls_self_signed_cert" "trustanchor_cert" {
+#   key_algorithm         = tls_private_key.trustanchor_key.algorithm
+#   private_key_pem       = tls_private_key.trustanchor_key.private_key_pem
+#   validity_period_hours = 87600
+#   is_ca_certificate     = true
 
-  values = [
-    "${file("${path.module}/linkerd_values.yaml")}"
-  ]
+#   subject {
+#     common_name    = "identity.linkerd.cluster.local"
+#     street_address = []
+#   }
 
-  set {
-    name  = "identityTrustAnchorsPEM"
-    value = tls_self_signed_cert.trustanchor_cert.cert_pem
-  }
+#   allowed_uses = [
+#     "crl_signing",
+#     "cert_signing",
+#     "server_auth",
+#     "client_auth"
+#   ]
+# }
 
-  set {
-    name  = "identity.issuer.crtExpiry"
-    value = tls_locally_signed_cert.issuer_cert.validity_end_time
-  }
+# resource "tls_private_key" "issuer_key" {
+#   algorithm   = "ECDSA"
+#   ecdsa_curve = "P256"
+# }
 
-  set {
-    name  = "identity.issuer.tls.crtPEM"
-    value = tls_locally_signed_cert.issuer_cert.cert_pem
-  }
+# resource "tls_cert_request" "issuer_req" {
+#   key_algorithm   = tls_private_key.issuer_key.algorithm
+#   private_key_pem = tls_private_key.issuer_key.private_key_pem
 
-  set {
-    name  = "identity.issuer.tls.keyPEM"
-    value = tls_private_key.issuer_key.private_key_pem
-  }
-}
+#   subject {
+#     common_name    = "identity.linkerd.cluster.local"
+#     street_address = []
+#   }
+# }
 
-resource "helm_release" "linkerd-viz" {
-  name       = "linkerd-viz"
-  repository = "https://helm.linkerd.io/stable"
-  chart      = "linkerd-viz"
+# resource "tls_locally_signed_cert" "issuer_cert" {
+#   cert_request_pem      = tls_cert_request.issuer_req.cert_request_pem
+#   ca_key_algorithm      = tls_private_key.trustanchor_key.algorithm
+#   ca_private_key_pem    = tls_private_key.trustanchor_key.private_key_pem
+#   ca_cert_pem           = tls_self_signed_cert.trustanchor_cert.cert_pem
+#   validity_period_hours = 8760
+#   is_ca_certificate     = true
 
-  depends_on = [
-    helm_release.linkerd
-  ]
+#   allowed_uses = [
+#     "crl_signing",
+#     "cert_signing",
+#     "server_auth",
+#     "client_auth"
+#   ]
+# }
 
-  values = [
-    "${file("${path.module}/linkerd_viz_values.yaml")}"
-  ]
-}
+# resource "helm_release" "linkerd" {
+#   name       = "linkerd"
+#   repository = "https://helm.linkerd.io/stable"
+#   chart      = "linkerd2"
+
+#   depends_on = [
+#     helm_release.cilium
+#   ]
+
+#   values = [
+#     "${file("${path.module}/linkerd_values.yaml")}"
+#   ]
+
+#   set {
+#     name  = "identityTrustAnchorsPEM"
+#     value = tls_self_signed_cert.trustanchor_cert.cert_pem
+#   }
+
+#   set {
+#     name  = "identity.issuer.crtExpiry"
+#     value = tls_locally_signed_cert.issuer_cert.validity_end_time
+#   }
+
+#   set {
+#     name  = "identity.issuer.tls.crtPEM"
+#     value = tls_locally_signed_cert.issuer_cert.cert_pem
+#   }
+
+#   set {
+#     name  = "identity.issuer.tls.keyPEM"
+#     value = tls_private_key.issuer_key.private_key_pem
+#   }
+# }
+
+# resource "helm_release" "linkerd-viz" {
+#   name       = "linkerd-viz"
+#   repository = "https://helm.linkerd.io/stable"
+#   chart      = "linkerd-viz"
+
+#   depends_on = [
+#     helm_release.linkerd
+#   ]
+
+#   values = [
+#     "${file("${path.module}/linkerd_viz_values.yaml")}"
+#   ]
+# }
