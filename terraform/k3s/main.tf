c719ee1281abb5281708b54319419d8ea73a5540
geraldwuhoo <gerald@geraldwu.com>
2024-09-05 21:11:23 -0700

    update k3s version in terraform; fix notify-url formatting in cloud-init

--- terraform/k3s/main.tf
+++ terraform/k3s/main.tf
@@ -56,7 +56,7 @@ module "k3s" {
   domain       = "wuhoo.xyz"
   vip_hostname = "k3s"
 
-  k3s_version = "v1.28.5+k3s1"
+  k3s_version = "v1.30.3+k3s1"
 
   template = "microos-template"
   start_id = 3000
