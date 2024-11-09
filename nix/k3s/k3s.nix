aeacb69e484b35c7c19472f675c1d2a8ed777be2
geraldwuhoo <gerald@geraldwu.com>
2024-11-08 22:53:28 -0800

    allow for clusterinit in k3s module

--- nix/k3s/k3s.nix
+++ nix/k3s/k3s.nix
@@ -12,6 +12,11 @@
         type = types.bool;
         description = "Whether or not this is a master node";
       };
+      clusterInit = mkOption {
+        type = types.bool;
+        description = "Whether or not to initialize the HA cluster";
+        default = false;
+      };
       singleNode = mkOption {
         type = types.bool;
         description = "Whether or not this is a single node cluster";
@@ -25,28 +30,18 @@
       [
         ceph-client # ceph kernel modules for ceph-csi
       ]
-      ++ (
-        if config.k3s.master then
-          [
-            etcd # for etcdctl
-          ]
-        else
-          [ ]
-      );
+      ++ (lib.optionals (config.k3s.master) [
+        etcd # for etcdctl
+      ]);
 
     programs.bash.shellAliases =
       {
         k = "kubectl";
         dropcaches = "sync; echo 3 > /proc/sys/vm/drop_caches";
       }
-      // (
-        if config.k3s.master then
-          {
-            ketcdctl = "etcdctl --cacert=/var/lib/rancher/k3s/server/tls/etcd/server-ca.crt --cert=/var/lib/rancher/k3s/server/tls/etcd/client.crt --key=/var/lib/rancher/k3s/server/tls/etcd/client.key";
-          }
-        else
-          { }
-      );
+      // (lib.mkIf (config.k3s.master) {
+        ketcdctl = "etcdctl --cacert=/var/lib/rancher/k3s/server/tls/etcd/server-ca.crt --cert=/var/lib/rancher/k3s/server/tls/etcd/client.crt --key=/var/lib/rancher/k3s/server/tls/etcd/client.key";
+      });
 
     boot.kernelModules = [
       # ceph for ceph-csi
@@ -66,32 +61,26 @@
       [
         6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
       ]
-      ++ (
-        if config.k3s.singleNode then
-          [ ]
-        else
-          [
-            2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
-            2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
-            5001 # k3s: embedded Spigel registry mirror
-            10250 # k3s: metrics server
-          ]
-      );
-    networking.firewall.allowedUDPPorts =
-      if config.k3s.singleNode then
-        [ ]
-      else
-        [
-          8472 # k3s, flannel: required if using multi-node for inter-node networking
-        ];
+      ++ (lib.optionals (!config.k3s.singleNode) [
+        2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
+        2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+        5001 # k3s: embedded Spigel registry mirror
+        10250 # k3s: metrics server
+      ]);
+    networking.firewall.allowedUDPPorts = lib.mkIf (!config.k3s.singleNode) [
+      8472 # k3s, flannel: required if using multi-node for inter-node networking
+    ];
 
     sops.secrets.k3s-token = { };
 
     services.k3s = {
       enable = true;
       role = if config.k3s.master then "server" else "agent";
-      tokenFile = if config.k3s.singleNode then null else config.sops.secrets.k3s-token.path;
-      serverAddr = if config.k3s.singleNode then "" else "https://k3s.wuhoo.xyz:6443";
+      tokenFile = lib.mkIf (!config.k3s.singleNode) config.sops.secrets.k3s-token.path;
+      serverAddr = lib.mkIf (
+        !config.k3s.singleNode && !config.k3s.clusterInit
+      ) "https://k3s.wuhoo.xyz:6443";
+      clusterInit = config.k3s.clusterInit;
 
       extraFlags = toString (
         [ ]
