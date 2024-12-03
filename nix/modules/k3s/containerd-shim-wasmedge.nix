edccbc2c5a85de75ac07a683f425e79c4e8ae321
geraldwuhoo <gerald@geraldwu.com>
2024-12-02 23:25:08 -0800

    add k3s static manifests

--- nix/modules/k3s/containerd-shim-wasmedge.nix
+++ nix/modules/k3s/containerd-shim-wasmedge.nix
@@ -1,42 +1,53 @@
-{ pkgs, lib, stdenv }:
+{
+  pkgs,
+  lib,
+  stdenv,
+}:
 
 let
   inherit (stdenv.hostPlatform) system;
   throwSystem = "Unsupported system: ${system}";
 
-  plat = {
-    x86_64-linux = "x86_64";
-    aarch64-linux = "aarch64";
-  }.${system} or throwSystem;
+  plat =
+    {
+      x86_64-linux = "x86_64";
+      aarch64-linux = "aarch64";
+    }
+    .${system} or throwSystem;
 
-  sha256 = {
-    x86_64-linux = "0vxkqqm6pfw5php0pj5j06rgsa748q27f8l97rs3kbc5ykmi5k73";
-    aarch64-linux = "1l0jhpqdnxxnwqr6qixc8ai4zymcpxc4a3ca2lwyc7akbpw2hj99";
-  }.${system} or throwSystem;
+  sha256 =
+    {
+      x86_64-linux = "0vxkqqm6pfw5php0pj5j06rgsa748q27f8l97rs3kbc5ykmi5k73";
+      aarch64-linux = "1l0jhpqdnxxnwqr6qixc8ai4zymcpxc4a3ca2lwyc7akbpw2hj99";
+    }
+    .${system} or throwSystem;
 in
 pkgs.stdenv.mkDerivation rec {
-	pname = "containerd-shim-wasmedge";
-	version = "0.4.0";
+  pname = "containerd-shim-wasmedge";
+  version = "0.4.0";
 
-	src = builtins.fetchurl {
-		url = "https://github.com/containerd/runwasi/releases/download/containerd-shim-wasmedge%2F${version}/containerd-shim-wasmedge-${plat}.tar.gz";
-		inherit sha256;
-	};
+  src = builtins.fetchurl {
+    url = "https://github.com/containerd/runwasi/releases/download/containerd-shim-wasmedge%2F${version}/containerd-shim-wasmedge-${plat}.tar.gz";
+    inherit sha256;
+  };
 
-	doCheck = false;
+  doCheck = false;
 
-	dontUnpack = true;
+  dontUnpack = true;
 
-	installPhase = ''
+  installPhase = ''
     echo $src $out
     mkdir -p $out
     tar -xzf $src -C $out
-	'';
+  '';
 
-	meta = with lib; {
-		homepage = "https://github.com/containerd/runwasi";
-		description = "wasm";
-		platforms = ["x86_64-linux" "aarch64-linux"];
-		license = licenses.asl20;
-	};
+  meta = with lib; {
+    homepage = "https://github.com/containerd/runwasi";
+    description = "wasm";
+    platforms = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+    license = licenses.asl20;
+  };
 }
--- nix/modules/k3s/k3s.nix
+++ nix/modules/k3s/k3s.nix
@@ -25,159 +25,165 @@
     };
   };
   config =
-  let
-    containerd-shim-wasmedge = pkgs.callPackage ./containerd-shim-wasmedge.nix {inherit pkgs lib;};
-  in
-  {
-    boot = {
-      kernel.sysctl = {
-        "fs.inotify.max_user_instances" = 8192;
-        "fs.inotify.max_user_watches" = 524288;
+    let
+      containerd-shim-wasmedge = pkgs.callPackage ./containerd-shim-wasmedge.nix { inherit pkgs lib; };
+    in
+    {
+      boot = {
+        kernel.sysctl = {
+          "fs.inotify.max_user_instances" = 8192;
+          "fs.inotify.max_user_watches" = 524288;
+        };
+        kernelModules = [
+          # ceph for ceph-csi
+          "ceph"
+          "rbd"
+
+          # IPVS for kube-vip
+          "ip_vs"
+          "ip_vs_rr"
+
+          # Wireguard for VPN
+          "tun"
+          "wireguard"
+        ];
       };
-      kernelModules = [
-        # ceph for ceph-csi
-        "ceph"
-        "rbd"
 
-        # IPVS for kube-vip
-        "ip_vs"
-        "ip_vs_rr"
-
-        # Wireguard for VPN
-        "tun"
-        "wireguard"
-      ];
-    };
-
-    environment.systemPackages =
-      with pkgs;
-      [
-        containerd-shim-wasmedge
-        ceph-client # ceph kernel modules for ceph-csi
-      ]
-      ++ (lib.optionals (config.k3s.master) [
-        etcd # for etcdctl
-      ]);
-
-    programs.bash.shellAliases =
-      {
-        k = "kubectl";
-        dropcaches = "sync; echo 3 > /proc/sys/vm/drop_caches";
-      }
-      // (lib.optionalAttrs (config.k3s.master) {
-        ketcdctl = "etcdctl --cacert=/var/lib/rancher/k3s/server/tls/etcd/server-ca.crt --cert=/var/lib/rancher/k3s/server/tls/etcd/client.crt --key=/var/lib/rancher/k3s/server/tls/etcd/client.key";
-      });
-
-    networking.firewall = {
-      allowedTCPPorts =
+      environment.systemPackages =
+        with pkgs;
         [
-          6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
+          containerd-shim-wasmedge
+          ceph-client # ceph kernel modules for ceph-csi
         ]
-        ++ (lib.optionals (!config.k3s.singleNode) [
-          2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
-          2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
-          5001 # k3s: embedded Spigel registry mirror
-          10250 # k3s: metrics server
+        ++ (lib.optionals (config.k3s.master) [
+          etcd # for etcdctl
         ]);
-      allowedUDPPorts = lib.mkIf (!config.k3s.singleNode) [
-        8472 # k3s, flannel: required if using multi-node for inter-node networking
-      ];
-    };
 
-    sops.secrets.k3s-token = { };
+      programs.bash.shellAliases =
+        {
+          k = "kubectl";
+          dropcaches = "sync; echo 3 > /proc/sys/vm/drop_caches";
+        }
+        // (lib.optionalAttrs (config.k3s.master) {
+          ketcdctl = "etcdctl --cacert=/var/lib/rancher/k3s/server/tls/etcd/server-ca.crt --cert=/var/lib/rancher/k3s/server/tls/etcd/client.crt --key=/var/lib/rancher/k3s/server/tls/etcd/client.key";
+        });
 
-    systemd.services.createContainerdShimWasmedge = {
-      description = "Create symlink for containerd-shim-wasmedge";
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
+      networking.firewall = {
+        allowedTCPPorts =
+          [
+            6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
+          ]
+          ++ (lib.optionals (!config.k3s.singleNode) [
+            2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
+            2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+            5001 # k3s: embedded Spigel registry mirror
+            10250 # k3s: metrics server
+          ]);
+        allowedUDPPorts = lib.mkIf (!config.k3s.singleNode) [
+          8472 # k3s, flannel: required if using multi-node for inter-node networking
+        ];
       };
-      script = ''
-      ${pkgs.coreutils-full}/bin/ln -sfn ${containerd-shim-wasmedge}/containerd-shim-wasmedge-v1 $(${pkgs.coreutils-full}/bin/readlink /var/lib/rancher/k3s/data/current)/bin
-      '';
-      wantedBy = [ "multi-user.target" ];
-    };
 
-    services.k3s = {
-      enable = true;
-      package = pkgs.k3s_1_30;
-      role = if config.k3s.master then "server" else "agent";
-      tokenFile = lib.mkIf (!config.k3s.singleNode) config.sops.secrets.k3s-token.path;
-      serverAddr = lib.mkIf (
-        !config.k3s.singleNode && !config.k3s.clusterInit
-      ) "https://k3s.wuhoo.xyz:6443";
-      clusterInit = config.k3s.clusterInit;
+      sops.secrets.k3s-token = { };
 
-      # Add wasmedge runtime to k3s
-      containerdConfigTemplate = ''
-      # Base K3s config
-      {{ template "base" . }}
+      systemd.services.createContainerdShimWasmedge = {
+        description = "Create symlink for containerd-shim-wasmedge";
+        serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = true;
+        };
+        script = ''
+          ${pkgs.coreutils-full}/bin/ln -sfn ${containerd-shim-wasmedge}/containerd-shim-wasmedge-v1 $(${pkgs.coreutils-full}/bin/readlink /var/lib/rancher/k3s/data/current)/bin
+        '';
+        wantedBy = [ "multi-user.target" ];
+      };
 
-      # Add a custom runtime
-      [plugins."io.containerd.grpc.v1.cri".containerd.runtimes."wasmedge"]
-        runtime_type = "io.containerd.wasmedge.v1"
-      [plugins."io.containerd.grpc.v1.cri".containerd.runtimes."wasmedge".options]
-        BinaryName = "${containerd-shim-wasmedge}/containerd-shim-wasmedge-v1"
-      '';
+      services.k3s = {
+        enable = true;
+        package = pkgs.k3s_1_30;
+        role = if config.k3s.master then "server" else "agent";
+        tokenFile = lib.mkIf (!config.k3s.singleNode) config.sops.secrets.k3s-token.path;
+        serverAddr = lib.mkIf (
+          !config.k3s.singleNode && !config.k3s.clusterInit
+        ) "https://k3s.wuhoo.xyz:6443";
+        clusterInit = config.k3s.clusterInit;
 
-      extraFlags = toString (
-        [
-          "--kubelet-arg=allowed-unsafe-sysctls=net.ipv6.conf.all.disable_ipv6,net.ipv6.conf.default.disable_ipv6,net.ipv4.ip_forward,net.ipv4.conf.all.src_valid_mark"
-        ]
-        ++ (
-          if config.k3s.master then
-            [
-              "--disable traefik"
-              "--disable local-storage"
-              "--disable-cloud-controller"
-              "--disable-helm-controller"
-              "--kubelet-arg node-status-update-frequency=5s"
-              "--kube-controller-manager-arg node-monitor-period=5s"
-              "--kube-controller-manager-arg node-monitor-grace-period=20s"
-            ]
-            ++ (
-              if config.k3s.singleNode then
-                [ "--tls-san ${config.networking.hostName}.wuhoo.xyz" ]
-              else
-                [
-                  "--tls-san k3s.wuhoo.xyz"
-                  "--disable servicelb"
-                  "--kube-controller-manager-arg bind-address=0.0.0.0"
-                  "--kube-proxy-arg metrics-bind-address=0.0.0.0"
-                  "--kube-scheduler-arg bind-address=0.0.0.0"
-                  "--etcd-expose-metrics"
-                  "--embedded-registry"
-                ]
-            )
-          else
-            [ ]
-        )
-      );
+        # Add wasmedge runtime to k3s
+        containerdConfigTemplate = ''
+          # Base K3s config
+          {{ template "base" . }}
 
-      configPath = lib.mkIf (
-        config.k3s.master && !config.k3s.singleNode
-      ) config.sops.templates.k3s-config.path;
-    };
+          # Add a custom runtime
+          [plugins."io.containerd.grpc.v1.cri".containerd.runtimes."wasmedge"]
+            runtime_type = "io.containerd.wasmedge.v1"
+          [plugins."io.containerd.grpc.v1.cri".containerd.runtimes."wasmedge".options]
+            BinaryName = "${containerd-shim-wasmedge}/containerd-shim-wasmedge-v1"
+        '';
 
-    sops.secrets.aws-access-key-id = lib.mkIf (config.k3s.master && !config.k3s.singleNode) { };
-    sops.secrets.aws-secret-access-key = lib.mkIf (config.k3s.master && !config.k3s.singleNode) { };
-    sops.templates.k3s-config = lib.mkIf (config.k3s.master && !config.k3s.singleNode) {
-      content = ''
-        etcd-s3: true
-        etcd-s3-endpoint: s3.wuhoo.xyz
-        etcd-s3-bucket: backup
-        etcd-s3-folder: etcd
-        etcd-s3-access-key: ${config.sops.placeholder.aws-access-key-id}
-        etcd-s3-secret-key: ${config.sops.placeholder.aws-secret-access-key}
-      '';
-    };
+        extraFlags = toString (
+          [
+            "--kubelet-arg=allowed-unsafe-sysctls=net.ipv6.conf.all.disable_ipv6,net.ipv6.conf.default.disable_ipv6,net.ipv4.ip_forward,net.ipv4.conf.all.src_valid_mark"
+          ]
+          ++ (
+            if config.k3s.master then
+              [
+                "--disable traefik"
+                "--disable local-storage"
+                "--disable-cloud-controller"
+                "--disable-helm-controller"
+                "--kubelet-arg node-status-update-frequency=5s"
+                "--kube-controller-manager-arg node-monitor-period=5s"
+                "--kube-controller-manager-arg node-monitor-grace-period=20s"
+              ]
+              ++ (
+                if config.k3s.singleNode then
+                  [ "--tls-san ${config.networking.hostName}.wuhoo.xyz" ]
+                else
+                  [
+                    "--tls-san k3s.wuhoo.xyz"
+                    "--disable servicelb"
+                    "--kube-controller-manager-arg bind-address=0.0.0.0"
+                    "--kube-proxy-arg metrics-bind-address=0.0.0.0"
+                    "--kube-scheduler-arg bind-address=0.0.0.0"
+                    "--etcd-expose-metrics"
+                    "--embedded-registry"
+                  ]
+              )
+            else
+              [ ]
+          )
+        );
 
-    environment.etc = {
-      # Enable both embedded Spigel registry and external registry mirrors
-      "rancher/k3s/registries.yaml" = {
-        source = ./manifests/registries.yaml;
-        mode = "0644";
+        configPath = lib.mkIf (
+          config.k3s.master && !config.k3s.singleNode
+        ) config.sops.templates.k3s-config.path;
+
+        manifests = lib.mkIf (!config.k3s.singleNode) (builtins.mapAttrs (name: file: { source = ./manifests/${name}.yaml; }) {
+          kube-vip-rbac = { };
+          kube-vip-ds = { };
+          kube-vip-worker-ds = { };
+        });
+      };
+
+      sops.secrets.aws-access-key-id = lib.mkIf (config.k3s.master && !config.k3s.singleNode) { };
+      sops.secrets.aws-secret-access-key = lib.mkIf (config.k3s.master && !config.k3s.singleNode) { };
+      sops.templates.k3s-config = lib.mkIf (config.k3s.master && !config.k3s.singleNode) {
+        content = ''
+          etcd-s3: true
+          etcd-s3-endpoint: s3.wuhoo.xyz
+          etcd-s3-bucket: backup
+          etcd-s3-folder: etcd
+          etcd-s3-access-key: ${config.sops.placeholder.aws-access-key-id}
+          etcd-s3-secret-key: ${config.sops.placeholder.aws-secret-access-key}
+        '';
+      };
+
+      environment.etc = {
+        # Enable both embedded Spigel registry and external registry mirrors
+        "rancher/k3s/registries.yaml" = {
+          source = ./manifests/registries.yaml;
+          mode = "0644";
+        };
       };
     };
-  };
 }
