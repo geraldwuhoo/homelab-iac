0b56ef8f9b6ad35fe4f09af6f56f39b32fe1d060
geraldwuhoo <gerald@geraldwu.com>
2026-09-13 23:15:01 -0700

    reach hetzner kube api through ssh socks proxy

--- terraform/modules/k3s-hcloud/outputs.tf
+++ terraform/modules/k3s-hcloud/outputs.tf
@@ -5,7 +5,8 @@ output "k3s_kubeconfig" {
         replace(data.external.kubeconfig.result.kubeconfig, " ", "")
       ),
       "server: https://127.0.0.1:6443",
-      "server: https://${var.name}:6443",
+      # The API is not exposed publicly; reach it through a local SOCKS proxy over SSH
+      "server: https://${var.name}:6443\n    proxy-url: socks5://127.0.0.1:1080",
     ),
     "default",
     var.name,
